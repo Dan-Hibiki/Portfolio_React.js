eacc0a973d7a747f52f11d4add9dd3cf6d5e6ff9
unknown <villardan44@gmail.com>
2020-12-18 11:23:44 +0100

    My First Commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Hibiki from './Hibiki';
+
+class Header extends Component {
+    render() {
+        return(
+            <div>
+                <Hibiki />
+            </div>
+        )
+    }
+}
+
+export default Header;
---
+++ src/Hibiki.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import hibiki from './hibiki.png';
+
+const Hibiki = () => {
+    return <img src={hibiki} alt="" />
+}
+
+export default Hibiki;
